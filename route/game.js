9042104acc71460f380b915be92bf867270f8da0
Suleiman Mamman <suleigolden>
2021-01-21 07:52:30 +0100

    game start route created

--- route/game.js
+++ route/game.js
@@ -2,9 +2,15 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-   
 
-    res.send("Hello Glden..");
+    res.send("<h4>****Hello, Welcome to TheLastCodeBender ABC 123 Game!****</h4><a href='game/start'>Start Game</a>");
+  
+});
+
+router.get('/start', (req, res)=>{
+    
+    res.send("<h4>If A=1 and B=2, and 3=C. What is 4</h4>");
+  
 });
 
 export default router;

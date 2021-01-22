8dc459f7736b98e5c07067fab435e6213e9355e2
Suleiman Mamman <suleigolden>
2021-01-22 15:22:25 +0100

    Check user answer logic created

--- route/game.js
+++ route/game.js
@@ -3,37 +3,13 @@ const router = express.Router();
 
 const characters_Answers = [
     {
-        A: "1",
-        B: "2",
-        C: "3",
-        D: "4",
-        E: "5",
-        F: "6",
-        G: "7",
-        H: "8",
-        I: "9",
-        J: "10",
-        K: "11",
-        L: "12",
-        M: "13",
-        N: "14",
-        O: "15",
-        P: "16",
-        Q: "17",
-        R: "18",
-        S: "19",
-        T: "20",
-        U: "21",
-        V: "22",
-        W: "23",
-        X: "24",
-        Y: "25",
-        Z: "26",
+        A: "1", B: "2", C: "3", D: "4", E: "5",F: "6",G: "7", H: "8", I: "9", J: "10",
+        K: "11", L: "12", M: "13", N: "14", O: "15", P: "16", Q: "17", R: "18", S: "19",
+        T: "20", U: "21", V: "22", W: "23", X: "24", Y: "25", Z: "26",
     }
 ];
 
 router.get('/', (req, res)=>{
-console.log(':: '+characters_Answers['A']);
 
     res.send("<h4>****Hello, Welcome to TheLastCodeBender ABC 123 Game!****</h4><a href='game/start'>Start Game</a>");
   
@@ -43,11 +19,30 @@ router.get('/start', (req, res)=>{
   
 });
 
+function checkAnswer(answer){
+
+    for(let i = 0; i < characters_Answers.length; i++) {
+        const obj = characters_Answers[i];
+        console.log('::: '+obj[answer]);
+        if(obj[answer] == answer){
+            //console.log(obj[answer]);
+            return true;
+        }
+        
+    }
+
+    return false;
+}
+
 router.get('/start:answer', (req, res)=>{
 const { answer } = req.params;
-    if(answer == "4"){
+const check_answer = checkAnswer(answer);
+ if(check_answer){
+     console.log('True');
+ }else{
+     console.log('false');
+ }
 
-    }
 });
 
 export default router;
